5731309c9b5973965adac4863ec3e5f73dc2d12e
Daniel Espinal <dancancode@protonmail.com>
2022-11-14 17:58:42 -0500

    all test passing

--- src/App.js
+++ src/App.js
@@ -1,90 +1,90 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [sound, setSound] = useState("");
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      playSound(event.key.toUpperCase());
+    });
+  }, []);
+
+  const keyPad = [
+    {
+      keyCode: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  function playSound(selector) {
+    const audio = document.getElementById(selector);
+    if (audio) {
+      setSound(`${selector}-pad`);
+      audio.play();
+    }
+  }
+
   return (
     <div id="drum-machine">
-      <div id="display"></div>
+      <div id="display">{sound}</div>
 
-      <button id="Q" className="drum-pad">
-        Q
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-          className="clip"
-          id="Q"
-        ></audio>
-      </button>
-
-      <button id="W" className="drum-pad">
-        W
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-          className="clip"
-          id="W"
-        ></audio>
-      </button>
-
-      <button id="E" className="drum-pad">
-        E
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-          className="clip"
-          id="E"
-        ></audio>
-      </button>
-
-      <button id="A" className="drum-pad">
-        A
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-          className="clip"
-          id="A"
-        ></audio>
-      </button>
-
-      <button id="S" className="drum-pad">
-        S
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-          className="clip"
-          id="S"
-        ></audio>
-      </button>
-
-      <button id="D" className="drum-pad">
-        D
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-          className="clip"
-          id="D"
-        ></audio>
-      </button>
-
-      <button id="Z" className="drum-pad">
-        Z
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-          className="clip"
-          id="Z"
-        ></audio>
-      </button>
-
-      <button id="X" className="drum-pad">
-        X
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-          className="clip"
-          id="X"
-        ></audio>
-      </button>
-
-      <button id="C" className="drum-pad">
-        C
-        <audio
-          src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-          className="clip"
-          id="C"
-        ></audio>
-      </button>
+      <div className="key-pad">
+        {keyPad.map((drum, index) => {
+          return (
+            <button
+              onClick={() => playSound(drum.text)}
+              key={index}
+              id={`${drum.text}-pad`}
+              className="drum-pad"
+            >
+              {drum.text}
+              <audio src={drum.src} className="clip" id={drum.text}></audio>
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
